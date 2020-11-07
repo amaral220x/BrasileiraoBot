267f0be848706ad4f718dbec7e92bb27c1440e8a
amaral220x <biel.ilha2021@gmail.com>
2020-11-07 14:54:22 -0300

    Criando o caso do Brasileirão com a key real

--- bot.py
+++ bot.py
@@ -50,6 +50,20 @@ async def campeonato(ctx, *args):
         embed.add_field(name='Status', value=datastore['status'])
         embed.set_footer(text=time.strftime("%X %d/%m/%Y"))
         await ctx.send(embed=embed)
+    elif arg == 'brasileiro' or arg == 'brasileirão' or arg == 'brasileirao':
+        url = 'https://api.api-futebol.com.br/v1/campeonatos/10'
+        head = {'Authorization': 'Bearer ' + keys.get_api_key()}
+        r = requests.get(url, headers=head)
+        r = r.text
+        datastore = json.loads(r)
+        embed = discord.Embed(colour = discord.Colour.blue())
+        embed.set_author(name="Campeonato", icon_url='https://logodownload.org/wp-content/uploads/2018/10/campeonato-brasileiro-logo-brasileirao-logo-5.png')
+        embed.add_field(name='Nome do campeonato', value=datastore['nome_popular'])
+        embed.add_field(name='Edição', value=datastore['edicao_atual']['temporada'])
+        embed.add_field(name='Tipo', value=datastore['tipo'])
+        embed.add_field(name='Rodada', value=datastore['rodada_atual']['nome'])
+        embed.add_field(name='Status', value=datastore['status'])
+
     embed.set_footer(text=time.strftime("%X %d/%m/%Y"))
     await ctx.send(embed=embed)
 
